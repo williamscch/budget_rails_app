3932226ec9bb61126c8a06aeadb4e20c9d2ace9d
wdavidcch <david.13.2121@gmail.com>
2022-11-14 18:24:37 -0400

    Add validation tests for the models

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validation tests for Expense model' do
+    it 'is not valid without a name' do
+      @expense1 = Expense.create(name: nil, amount: 100)
+      expect(@expense1).to_not be_valid
+    end
+
+    it 'is not valid without an amount' do
+      @expense1 = Expense.create(name: 'name', amount: nil)
+      expect(@expense1).to_not be_valid
+    end
+  end
 end
